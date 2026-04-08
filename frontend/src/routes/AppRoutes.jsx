d207b8b12855523046a81d77085a5ef7ea5c1474
hmngoc <167313494+hmngoc@users.noreply.github.com>
2026-04-09 00:22:58 +0700

    feat: update cart page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '@/pages/HomePage';
 import UserRoutes from './UserRoutes';
+import CartPage from '../pages/user/cart/CartPage';
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/profile/*' element={<UserRoutes />} />
+            <Route path='/cart' element={<CartPage />} />
         </Routes>
     );
 }
